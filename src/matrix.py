300b87b9f8f4ba8ebcff93a7340b9d86b3994b33
KietsuKite9474 <hwang9@scu.edu>
2026-04-14 15:58:07 -0700

    Added modified code for signal clustering
    Modified code from base project for creation of similarity matrices and training csv for each essay set to match project folder structure. Adjusted to include a loop for sets 1-8. Also shifted some files between folders.

---
+++ src/matrix.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.sparse import csr_matrix
+from scipy.spatial.distance import cdist
+
+import json
+import pandas as pd 
+import numpy as np
+
+for ESSAY_SET in range(1, 9):
+    print(f"Processing essay set {ESSAY_SET}...")
+
+    df = pd.read_csv(f"../data/essay_set_{ESSAY_SET}.csv", index_col=0).reset_index()
+    df = df[df["split"] == "train"]
+
+    corpus = df.apply(lambda x: f"Essay: {x['essay']}", axis=1)
+
+    vectorizer = CountVectorizer(
+        lowercase=True,
+        binary=True,
+        analyzer='word',
+        min_df=5,
+        strip_accents=None
+    )
+
+    bow = vectorizer.fit_transform(corpus)
+
+    bool_bow = csr_matrix((bow.toarray() > 0).astype(np.uint8))
+
+    jaccard_distance = cdist(bool_bow.toarray(), bool_bow.toarray(), metric="jaccard")
+    jaccard_sim = 1 - jaccard_distance
+
+    np.save(f"../data/sim_matrix_{ESSAY_SET}.npy", jaccard_sim)
+
+    print(f"Saved sim_matrix_{ESSAY_SET}.npy")
---
+++ src/signal_pred.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import json
+from scipy import stats
+
+def get_z_values(S0, sim, max_iter=100, eps=1e-5):
+    S = S0
+    S_std = S.std()
+    if S_std == 0:
+        S_std = 1
+
+    Z = np.array([(S[k] - np.concatenate([S[:k], S[k+1:]]).mean()) / S_std
+            for k in range(S.shape[0])])
+
+    corr = 0
+    corr_per_iter = [corr]
+
+    for _ in range(max_iter):
+        S = sim @ Z
+
+        if S.std() == 0:
+            break
+        
+        else:
+            Z1 = np.array([(S[k] - np.concatenate([S[:k], S[k+1:]]).mean()) / S.std()
+                for k in range(S.shape[0])])
+                
+            corr = abs(stats.pearsonr(Z, Z1)[0])
+            corr_per_iter.append(corr)
+            Z = Z1
+
+            if corr > 1 - eps:
+                break
+    return Z
+
+for ESSAY_SET in range(1, 9):
+    print(f"Processing essay set {ESSAY_SET}...")
+
+    df = pd.read_csv(f"../data/essay_set_{ESSAY_SET}.csv", index_col=0)
+    df = df[df["split"] == "train"].reset_index(drop=True)
+
+    indexs = df.index.to_numpy()
+
+    sim = np.load(f"../data/sim_matrix_{ESSAY_SET}.npy", allow_pickle=True)
+
+    y_guess = df["essay"].apply(
+        lambda x: len(str(x)) if str(x) != "nan" else 0
+    ).to_numpy()
+
+    N = sim.shape[0]
+    support_indexs = np.arange(N)
+
+    df_test = df[["essay_id"]].copy()
+
+    partition_p = support_indexs
+
+    sim_p = sim[np.ix_(partition_p, partition_p)]
+    np.fill_diagonal(sim_p, 0)
+
+    y_guess_p = y_guess[partition_p]
+
+    z_values_p = get_z_values(
+        S0=y_guess_p,
+        sim=sim_p,
+        max_iter=500,
+        eps=1e-6
+    )
+
+    corr = stats.pearsonr(y_guess_p, z_values_p)[0]
+    z_sign = np.sign(corr)
+    z_sign = z_sign if not np.isnan(z_sign) else 1
+
+    z_values = z_sign * z_values_p
+
+    df_test["pred"] = z_values
+    df_test.to_csv(f"../data/train_{ESSAY_SET}.csv", index=False)
