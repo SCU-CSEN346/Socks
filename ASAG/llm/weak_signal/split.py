3c0cc9dd22a35d6ee63e8fc54af96f80dbf81016
shravan <sdhanasekaran@scu.edu>
2026-05-14 14:57:34 -0700

    Delete ASAG directory

--- ASAG/llm/weak_signal/split.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/train_val.csv", index_col=0)
-df[df["split"] == "train"].to_csv("data/train.csv")
-df[df["split"] == "val"].to_csv("data/val.csv")
--- ASAG/ulra_paper/weak_signal/join_train_val.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-train = pd.read_csv("data/train.csv", index_col=0)
-val = pd.read_csv("data/val.csv", index_col=0)
-
-pd.concat([train, val], ignore_index=True).to_csv("data/train_val.csv")
